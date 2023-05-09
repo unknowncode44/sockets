f0c7415a30a11b5e4907dace063e6ad921136b1b
unknowncode44 <matiaz.orellana@gmail.com>
2023-05-09 20:10:14 -0300

    230509

--- public/index.js
+++ public/index.js
@@ -1,6 +1,18 @@
 const socket = io();
 
-socket.on('mi mensaje', data=>{
+socket.on('mi mensaje', data => {
     alert(data);
     socket.emit('notificacion', 'Mensaje recibido exitosamente')
 })
+
+function render(data) {
+    var html = data.map(function(elem, index) {
+        return (`<div><strong>${elem.author}</strong>
+        <em>${elem.author}<em></div>`)
+    }).join("");
+
+    document.getElementById('messages').innerHTML = html;
+
+
+}
+socket.on('messages', function(data) { render(data); })
